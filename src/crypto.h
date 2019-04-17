8329c0669ca2546ebf378ede95113165c5999050
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-04-17 18:10:59 -0400

    Change CRYPTO_HDR_LEN from #define to static

--- src/crypto.h
+++ src/crypto.h
@@ -4,16 +4,17 @@
 #include <openssl/evp.h>
 #include <assert.h>
 
-#define CRYPTO_IV_LEN 12
-#define CRYPTO_TAG_LEN 16
-
-#define CRYPTO_HDR_LEN (CRYPTO_IV_LEN + CRYPTO_TAG_LEN)
+#define CRYPTO_IV_LEN 12u
+#define CRYPTO_TAG_LEN 16u
 
 #define CRYPTO_VERBOSE 1
 
 namespace erpc {
 // Shared key for each eRPC session. This should _eventually_ be negotiated
 // using a secure handshake
+
+static const size_t CRYPTO_HDR_LEN { CRYPTO_IV_LEN + CRYPTO_TAG_LEN };
+
 static const unsigned char gcm_key[] = {
     0xee, 0xbc, 0x1f, 0x57, 0x48, 0x7f, 0x51, 0x92, 0x1c, 0x04, 0x65,
     0x66, 0x5f, 0x8a, 0xe6, 0xd1, 0x65, 0x8b, 0xb2, 0x6d, 0xe6, 0xf8,
